4bd66942eab4c5be8eaecd61fa1eb90f5201b2d4
elshaarawy <elshaarawy200825@hotmail.com>
2017-05-23 18:58:41 +0200

    feat: implement SQLite Db Helper

--- app/src/main/java/net/elshaarawy/mobilia/Data/MobiliaContract.java
+++ app/src/main/java/net/elshaarawy/mobilia/Data/MobiliaContract.java
@@ -1,6 +1,7 @@
 package net.elshaarawy.mobilia.Data;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 /**
  * Created by elshaarawy on 23-May-17.
@@ -11,15 +12,86 @@ public class MobiliaContract {
 
     public static final Uri BASE_CONTENT_URI = new Uri.Builder().scheme("content").authority(AUTHORITY).build();
 
-    public interface Paths{
+    public interface CategoriesColumns extends BaseColumns {
+        String TABLE_NAME = "categories";
+
+        String _ID = "c_id";
+        String TITLE = "c_title";
+
+        int INDEX_ID = 0;
+        int INDEX_TITLE = 1;
+    }
+
+    public interface FurnitureColumns extends BaseColumns {
+
+        String TABLE_NAME = "furniture";
+
+        String _ID = "f_id";
+        String TITLE = "f_title";
+        String CATEGORY_ID = "f_category_id";
+        String SHOP_ID = "f_shop_id";
+        String IMAGE = "f_image";
+        String BODY = "f_body";
+
+        int INDEX_ID = 0;
+        int INDEX_TITLE = 1;
+        int INDEX_CATEGORY_ID = 2;
+        int INDEX_SHOP_ID = 3;
+        int INDEX_IMAGE = 4;
+        int INDEX_BODY = 5;
+    }
+
+    public interface ShopsColumns extends BaseColumns{
+
+        String TABLE_NAME = "shops";
+
+        String _ID = "s_id";
+        String TITLE = "s_title";
+        String CATEGORY_ID = "s_category_id";
+        String IMAGE = "s_image";
+        String PHONE = "s_phone";
+        String WEBSITE = "s_website";
+        String ABOUT = "s_about";
+
+        int INDEX_ID = 0;
+        int INDEX_TITLE = 1;
+        int INDEX_CATEGORY_ID = 2;
+        int INDEX_IMAGE = 3;
+        int INDEX_PHONE = 4;
+        int INDEX_WEBSITE = 5;
+        int INDEX_ABOUT = 6;
+
+    }
+
+    public interface OffersColumns extends BaseColumns {
+
+        String TABLE_NAME = "offers";
+
+        String _ID = "o_id";
+        String TITLE = "o_title";
+        String CATEGORY_ID = "o_category_id";
+        String SHOP_ID = "o_shop_id";
+        String IMAGE = "o_image";
+        String BODY = "o_body";
+
+        int INDEX_ID = 0;
+        int INDEX_TITLE = 1;
+        int INDEX_CATEGORY_ID = 2;
+        int INDEX_SHOP_ID = 3;
+        int INDEX_IMAGE = 4;
+        int INDEX_BODY = 5;
+    }
+
+
+    public interface Paths {
         //TODO declare paths Strings
     }
 
-    public interface ProviderUris{
+    public interface ProviderUris {
         //TODO Build Mobilia Provider Uris from Paths and authorities
     }
 
-    public interface MatchingCodes{
+    public interface MatchingCodes {
         //TODO declare Matching codes Integers for Uri Matcher
     }
 
--- app/src/main/java/net/elshaarawy/mobilia/Data/MobiliaDbHelper.java
+++ app/src/main/java/net/elshaarawy/mobilia/Data/MobiliaDbHelper.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static net.elshaarawy.mobilia.Data.MobiliaContract.CategoriesColumns;
+import static net.elshaarawy.mobilia.Data.MobiliaContract.FurnitureColumns;
+import static net.elshaarawy.mobilia.Data.MobiliaContract.ShopsColumns;
+import static net.elshaarawy.mobilia.Data.MobiliaContract.OffersColumns;
+
 /**
  * Created by elshaarawy on 23-May-17.
  */
@@ -19,17 +24,50 @@ public class MobiliaDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //TODO Create Databases using SQLTableCreator
+        db.execSQL(SQLTableCreator.CREATE_CATEGORIES);
+        db.execSQL(SQLTableCreator.CREATE_FURNITURE);
+        db.execSQL(SQLTableCreator.CREATE_SHOPS);
+        db.execSQL(SQLTableCreator.CREATE_OFFERS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //TODO Handle Database Upgrade
+        db.execSQL("DROP TABLE IF EXISTS " + CategoriesColumns.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + FurnitureColumns.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + ShopsColumns.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + OffersColumns.TABLE_NAME);
 
         this.onCreate(db);
     }
 
-    private interface SQLTableCreator{
-        //TODO declare SQLite Queries to Create tables
+    private interface SQLTableCreator {
+        String CREATE_CATEGORIES = "CREATE TABLE" + CategoriesColumns.TABLE_NAME + " ( " +
+                CategoriesColumns._ID + " INTEGER PRIMARY KEY , " +
+                CategoriesColumns.TITLE + " TEXT NOT NULL ); ";
+
+        String CREATE_FURNITURE = "CREATE TABLE" + FurnitureColumns.TABLE_NAME + " ( " +
+                FurnitureColumns._ID + " INTEGER PRIMARY KEY , " +
+                FurnitureColumns.TITLE + " TEXT NOT NULL , " +
+                FurnitureColumns.CATEGORY_ID + " INTEGER NOT NULL , " +
+                FurnitureColumns.SHOP_ID + " INTEGER NOT NULL , " +
+                FurnitureColumns.IMAGE + " TEXT NOT NULL , " +
+                FurnitureColumns.BODY + " TEXT NOT NULL ); ";
+
+        String CREATE_SHOPS = "CREATE TABLE" + ShopsColumns.TABLE_NAME + " ( " +
+                ShopsColumns._ID + " INTEGER PRIMARY KEY , " +
+                ShopsColumns.TITLE + " TEXT NOT NULL , " +
+                ShopsColumns.CATEGORY_ID + " INTEGER NOT NULL , " +
+                ShopsColumns.IMAGE + " TEXT NOT NULL , " +
+                ShopsColumns.PHONE + " TEXT NOT NULL , " +
+                ShopsColumns.WEBSITE + " TEXT NOT NULL , " +
+                ShopsColumns.ABOUT + " TEXT NOT NULL ); ";
+
+        String CREATE_OFFERS = "CREATE TABLE" + OffersColumns.TABLE_NAME + " ( " +
+                OffersColumns._ID + " INTEGER PRIMARY KEY , " +
+                OffersColumns.TITLE + " TEXT NOT NULL , " +
+                OffersColumns.CATEGORY_ID + " INTEGER NOT NULL , " +
+                OffersColumns.SHOP_ID + " INTEGER NOT NULL , " +
+                OffersColumns.IMAGE + " TEXT NOT NULL , " +
+                OffersColumns.BODY + " TEXT NOT NULL ); ";
     }
 }
